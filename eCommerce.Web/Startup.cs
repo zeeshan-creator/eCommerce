143e8c330b6b8ca988d33478485dda1b883f1815
ZEESHAN KHAN <zeekhan9753@outlook.com>
2020-10-02 16:02:23 +0500

    Add project files.

---
+++ eCommerce.Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eCommerce.Web.Startup))]
+
+namespace eCommerce.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
